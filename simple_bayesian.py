f020f0fbc2b972051c75eb0e6ef8ec574d19fc1a
Sergey Romanov <xxsmotur@gmail.com>
2016-01-29 23:58:17 +0500

    Add another model

--- simple_bayesian.py
+++ simple_bayesian.py
@@ -13,3 +13,15 @@ class Simple1:
         second = pymc.Bernoulli('S', p_first, value=pl.ones(self.obs))
         p_G = mc.Lambda('p_G', lambda S=S, R=R:pl.where(S, pl.where(R, .99, .9), pl.where(R, .8, 0.)),doc='Pr[G|S,R]')
         G = mc.Bernoulli('G', p_G, value=G_obs, observed=True)
+
+
+class Simple2:
+    ''' Another bayesian network
+    '''
+    def __init__(self, true_pA, true_pB):
+        self.true_pA = true_pA
+        self.true_pB = true_pB
+
+    def generate(self, N_A, N_B):
+        obsA = pymc.rbernoulli(self.true_pA, N_A)
+        obsB = pymc.rbernoulli(self.true_PB, N_B)
